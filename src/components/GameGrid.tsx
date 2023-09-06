ac282f55f8c25deddf4832c4e061de6767c62c6b
Tomas Pesek <pesekt@gmail.com>
2023-09-06 12:11:48 +0200

    added useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Image } from "@chakra-ui/image";
-
-interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-}
-
-interface GameRespose {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-
-  useEffect(() => {
-    apiClient.get<GameRespose>("games").then((response) => {
-      setGames(response.data.results);
-      console.log(response);
-    });
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
+      {error && <p>{error}</p>}
       {games.map((game) => (
         <div key={game.id}>
           <p>{game.name}</p>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface GameRespose {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<GameRespose>("games")
+      .then((response) => {
+        setGames(response.data.results);
+        console.log(response);
+      })
+      .catch((error) => setError(error.message));
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
