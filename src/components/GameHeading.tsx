c1c2f7e90615b2cdbea66bf4904601d41efbc326
Tomas Pesek <pesekt@gmail.com>
2023-10-11 12:17:25 +0200

    fixed GameHeading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,22 +1,15 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
+import useGenre from "../hooks/useGenre";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genreData } = useGenres();
-  const genre = genreData?.results.find(
-    (genre) => genre.id === gameQuery.genreId
-  );
-
-  const { data: platformData } = usePlatforms();
-  const platform = platformData?.results.find(
-    (platform) => platform.id === gameQuery.platformId
-  );
+  const genre = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
 
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
