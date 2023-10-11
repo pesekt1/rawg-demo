ab9e744b7d6cc52db30f47a4b0e43dd198b049f4
Tomas Pesek <pesekt@gmail.com>
2023-10-11 10:22:27 +0200

    moved FetchResponse interface to api-client

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import apiClient, { FetchResponse } from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-export interface FetchResponse<T> {
-  count: number;
-  results: T[];
-}
 //selectedGenre?: Genre | null
 const useData = <T>(
   endpont: string,
